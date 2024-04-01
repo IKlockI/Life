2eb56876ae4fd983a15fd379d867d8dc46af772f
IKlockI <165647274+IKlockI@users.noreply.github.com>
2024-04-01 11:28:36 +0300

    Add files via upload

---
+++ Игра жизнь/прога/LIFE/LIFE/Smertiorlife/Form1.Designer.cs
@@ -0,0 +1,204 @@
+﻿namespace Smertiorlife
+{
+    partial class LIFE
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(LIFE));
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.bblue = new System.Windows.Forms.Button();
+            this.bred = new System.Windows.Forms.Button();
+            this.bStop = new System.Windows.Forms.Button();
+            this.bStart = new System.Windows.Forms.Button();
+            this.nudDensity = new System.Windows.Forms.NumericUpDown();
+            this.label2 = new System.Windows.Forms.Label();
+            this.nudResolution = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.nudDensity)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudResolution)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            resources.ApplyResources(this.splitContainer1, "splitContainer1");
+            this.splitContainer1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.splitContainer1.FixedPanel = System.Windows.Forms.FixedPanel.Panel1;
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            resources.ApplyResources(this.splitContainer1.Panel1, "splitContainer1.Panel1");
+            this.splitContainer1.Panel1.BackColor = System.Drawing.Color.LavenderBlush;
+            this.splitContainer1.Panel1.Controls.Add(this.bblue);
+            this.splitContainer1.Panel1.Controls.Add(this.bred);
+            this.splitContainer1.Panel1.Controls.Add(this.bStop);
+            this.splitContainer1.Panel1.Controls.Add(this.bStart);
+            this.splitContainer1.Panel1.Controls.Add(this.nudDensity);
+            this.splitContainer1.Panel1.Controls.Add(this.label2);
+            this.splitContainer1.Panel1.Controls.Add(this.nudResolution);
+            this.splitContainer1.Panel1.Controls.Add(this.label1);
+            this.splitContainer1.Panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.splitContainer1_Panel1_Paint);
+            // 
+            // splitContainer1.Panel2
+            // 
+            resources.ApplyResources(this.splitContainer1.Panel2, "splitContainer1.Panel2");
+            this.splitContainer1.Panel2.Controls.Add(this.pictureBox1);
+            // 
+            // bblue
+            // 
+            resources.ApplyResources(this.bblue, "bblue");
+            this.bblue.BackColor = System.Drawing.Color.MidnightBlue;
+            this.bblue.Name = "bblue";
+            this.bblue.UseVisualStyleBackColor = false;
+            this.bblue.Click += new System.EventHandler(this.bblue_Click);
+            // 
+            // bred
+            // 
+            resources.ApplyResources(this.bred, "bred");
+            this.bred.BackColor = System.Drawing.Color.Crimson;
+            this.bred.Name = "bred";
+            this.bred.UseVisualStyleBackColor = false;
+            this.bred.Click += new System.EventHandler(this.bred_Click);
+            // 
+            // bStop
+            // 
+            resources.ApplyResources(this.bStop, "bStop");
+            this.bStop.BackColor = System.Drawing.Color.OrangeRed;
+            this.bStop.Name = "bStop";
+            this.bStop.UseVisualStyleBackColor = false;
+            this.bStop.Click += new System.EventHandler(this.Stop_Click);
+            // 
+            // bStart
+            // 
+            resources.ApplyResources(this.bStart, "bStart");
+            this.bStart.BackColor = System.Drawing.Color.Lime;
+            this.bStart.Name = "bStart";
+            this.bStart.UseVisualStyleBackColor = false;
+            this.bStart.Click += new System.EventHandler(this.Start_Click);
+            // 
+            // nudDensity
+            // 
+            resources.ApplyResources(this.nudDensity, "nudDensity");
+            this.nudDensity.Minimum = new decimal(new int[] {
+            2,
+            0,
+            0,
+            0});
+            this.nudDensity.Name = "nudDensity";
+            this.nudDensity.Value = new decimal(new int[] {
+            2,
+            0,
+            0,
+            0});
+            // 
+            // label2
+            // 
+            resources.ApplyResources(this.label2, "label2");
+            this.label2.Name = "label2";
+            // 
+            // nudResolution
+            // 
+            resources.ApplyResources(this.nudResolution, "nudResolution");
+            this.nudResolution.Maximum = new decimal(new int[] {
+            25,
+            0,
+            0,
+            0});
+            this.nudResolution.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.nudResolution.Name = "nudResolution";
+            this.nudResolution.Value = new decimal(new int[] {
+            3,
+            0,
+            0,
+            0});
+            // 
+            // label1
+            // 
+            resources.ApplyResources(this.label1, "label1");
+            this.label1.Name = "label1";
+            // 
+            // pictureBox1
+            // 
+            resources.ApplyResources(this.pictureBox1, "pictureBox1");
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_Mouse);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 40;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // GameLife
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.splitContainer1);
+            this.Name = "GameLife";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.GameLife_Load);
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel1.PerformLayout();
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.nudDensity)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudResolution)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.NumericUpDown nudResolution;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button bStop;
+        private System.Windows.Forms.Button bStart;
+        private System.Windows.Forms.NumericUpDown nudDensity;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button bblue;
+        private System.Windows.Forms.Button bred;
+    }
+}
+
---
+++ Игра жизнь/прога/LIFE/LIFE/Smertiorlife/Form1.cs
@@ -0,0 +1,214 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace Smertiorlife
+{
+    public partial class LIFE : Form
+    {
+        private int schetchikgeyzira = 0;
+        private Graphics Graphics;
+        private int resolution;
+        private int rows;
+        private int cols;
+        private int[,] field;   //Принимает значения от 0 до 2
+        int b = 0;
+
+        public LIFE()
+        {
+            InitializeComponent();
+        }
+        private int Red(int x, int y)
+        {
+            int count = 0;
+            for (int i = -1; i < 2; i++)
+            {
+                for (int e = -1; e < 2; e++)
+                {
+                    var col = (x + i + cols) % cols;
+                    var row = (y + e + rows) % rows;
+                    var isCheking = ((col == x) && (row == y));
+                    var lifeColor = field[col, row];
+                    if (lifeColor == 1 && !isCheking)
+                    {
+                        count++;
+                    }
+                }
+            }
+            return count;
+        }
+        private int Blue(int x, int y)
+        {
+            int count = 0;
+            for (int i = -1; i < 2; i++)
+            {
+                for (int e = -1; e < 2; e++)
+                {
+                    var col = (x + i + cols) % cols;
+                    var row = (y + e + rows) % rows;
+                    var isCheking = ((col == x) && (row == y));
+                    var lifeColor = field[col, row];
+                    if (lifeColor == 2 && !isCheking)
+                    {
+                        count++;
+                    }
+                }
+            }
+            return count;
+        }
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            Graphics.Clear(Color.Black);
+            bool newGeneration = false;
+            var newfield = new int[cols, rows]; // новое поле для положения клеток
+            for (int x = 0; x < cols; x++)
+            {
+                for (int y = 0; y < rows; y++)
+                {
+                    var Nightboarth = Red(x, y);
+                    var lifeColor = field[x, y];
+                    if (lifeColor == 1 && (Nightboarth == 2 || Nightboarth == 3))
+                    {
+                        newfield[x, y] = 1;
+                        Graphics.FillRectangle(Brushes.Red, x * resolution, y * resolution, resolution - 1, resolution - 1);
+                        continue;
+                    }
+                    else if (lifeColor == 1 && (Nightboarth > 2 || Nightboarth < 3))
+                    {
+                        newfield[x, y] = 0;
+                        Graphics.FillRectangle(Brushes.Red, x * resolution, y * resolution, resolution - 1, resolution - 1);
+                        continue;
+                    }
+                    if (lifeColor != 1 && Nightboarth == 3)
+                    {
+                        newfield[x, y] = 1;
+                        Graphics.FillRectangle(Brushes.Red, x * resolution, y * resolution, resolution - 1, resolution - 1);
+                        newGeneration = true;
+                        continue;
+                    }
+                }
+            }
+            for (int x = 0; x < cols; x++)
+            {
+                for (int y = 0; y < rows; y++)
+                {
+                    var Nightboarth = Blue(x, y);
+                    var lifeColor = field[x, y];
+                    if (lifeColor == 2 && (Nightboarth == 2 || Nightboarth == 3))
+                    {
+                        newfield[x, y] = 2;
+                        Graphics.FillRectangle(Brushes.Blue, x * resolution, y * resolution, resolution - 1, resolution - 1);
+                        continue;
+                    }
+                    else if (lifeColor == 2 && (Nightboarth > 2 || Nightboarth < 3))
+                    {
+                        newfield[x, y] = 0;
+                        Graphics.FillRectangle(Brushes.Blue, x * resolution, y * resolution, resolution - 1, resolution - 1);
+                        continue;
+                    }
+                    if (lifeColor != 2 && Nightboarth == 3)
+                    {
+                        newfield[x, y] = 2;
+                        Graphics.FillRectangle(Brushes.Blue, x * resolution, y * resolution, resolution - 1, resolution - 1);
+                        newGeneration = true;
+                        continue;
+                    }
+                }
+            }
+            if (newGeneration)
+            {
+                schetchikgeyzira++;
+            }
+            Text = $"Schetchik {schetchikgeyzira}";
+            field = newfield;
+            pictureBox1.Refresh();
+        }
+        private void Start_Click(object sender, EventArgs e)
+        {
+            if (timer1.Enabled)
+                return;
+            schetchikgeyzira = 0;
+            Text = $"Schetchik {schetchikgeyzira}";
+            nudResolution.Enabled = false;
+            nudDensity.Enabled = false;
+            resolution = (int)nudResolution.Value;
+            rows = pictureBox1.Height / resolution;
+            cols = pictureBox1.Width / resolution;
+            field = new int[cols, rows];
+            Random random = new Random();
+            for (int i = 0; i < cols; i++)
+            {
+                for (int j = 0; j < rows; j++)
+                {
+                    field[i, j] = random.Next(2);
+                }
+            }
+            pictureBox1.Image = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            Graphics = Graphics.FromImage(pictureBox1.Image);
+            timer1.Start();
+        }
+        private void Stop_Click(object sender, EventArgs e)
+        {
+            if (!timer1.Enabled)
+                return;
+            timer1.Stop();
+            nudResolution.Enabled = true;
+            nudDensity.Enabled = true;
+        }
+
+        private void pictureBox1_Mouse(object sender, MouseEventArgs e)
+        {
+            if (!timer1.Enabled)
+                return;
+            if (e.Button == MouseButtons.Left)
+            {
+                Random random = new Random();
+                var x = e.Location.X / resolution;
+                var y = e.Location.Y / resolution;
+                var ValidatePosition = MousePosition(x, y);
+                if (ValidatePosition)
+                    field[x, y] = b;
+            }
+            if (e.Button == MouseButtons.Right)
+            {
+                var x = e.Location.X / resolution;
+                var y = e.Location.Y / resolution;
+                var ValidatePosition = MousePosition(x, y);
+                if (ValidatePosition)
+                    field[x, y] = 0;
+            }
+        }
+        private bool MousePosition(int x, int y)
+        {
+            return x >= 0 && y >= 0 && x < cols && y < rows;
+        }
+
+        private void bred_Click(object sender, EventArgs e)
+        {
+            b = 1;
+        }
+
+        private void bblue_Click(object sender, EventArgs e)
+        {
+            b = 2;
+        }
+
+        private void GameLife_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void splitContainer1_Panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+    }
+}
+
